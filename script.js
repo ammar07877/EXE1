e702bf1713e6500bc4a400f56aa61fe990ce29d2
ammar07877 <ammarmahmd476@gmail.com>
2024-06-16 03:00:05 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const menu = document.querySelector('.dropdown-menu');
+    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+}
+
+function goToChannel() {
+    window.location.href = 'https://www.youtube.com/channel/UCLbwKNhLtwJgbju4HtUcQFA';
+}
